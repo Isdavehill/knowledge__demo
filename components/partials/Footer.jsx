998ce8f9020ef8fa2e953076876b50573cd9715e
Dave Hill <99537934+Isdavehill@users.noreply.github.com>
2023-02-07 23:00:59 +0000

    Add react-icons

--- components/partials/Footer.jsx
+++ components/partials/Footer.jsx
@@ -1,9 +1,11 @@
+import { FaHeart } from 'react-icons/fa';
+import { SiNextdotjs } from 'react-icons/si';
 const Year = new Date().getFullYear();
 function Footer() {
   return (
     <footer className="site-footer">
       <p>Copyright &copy; {Year} Dave Hill</p>
-      <p>created using Next.js</p>
+      <p>created with <span aria-label="love" className="heart"><FaHeart /></span> using <span aria-hidden="true"><SiNextdotjs /></span> Next.js</p>
     </footer>
     );
 }
