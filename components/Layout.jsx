5e206d1a106e145cd3aced821af0a0f8058cbe85
Dave Hill <99537934+Isdavehill@users.noreply.github.com>
2023-02-07 18:48:26 +0000

    Add Layout

---
+++ components/Layout.jsx
@@ -0,0 +1,16 @@
+import Header from "./partials/Header";
+import Footer from "./partials/Footer";
+
+function Layout(children) {
+  return (
+    <>
+      <Header />
+      <main>
+        {children}
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default Layout;
---
+++ components/partials/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer() {
+  return (
+    <footer className="footer">
+        <div className="container">
+    </footer>
+    );
+    }
+
+export default Footer;
