76892df63f49ec6f9dd2027c667b412a9df262ed
Dave Hill <99537934+Isdavehill@users.noreply.github.com>
2023-02-07 18:50:30 +0000

    Add custom script

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
+import Script from "next/script";
 import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }) {
     return (
     <Layout>
+        <Script src="../public/js/custom.js" />
         <Component {...pageProps} />
     </Layout>
 }
