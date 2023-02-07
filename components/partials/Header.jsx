fa0e803cce8b1dcdb7d84e7ab8ccc08cf4f6f2c8
Dave Hill <99537934+Isdavehill@users.noreply.github.com>
2023-02-07 23:00:38 +0000

    Add nav

--- components/partials/Header.jsx
+++ components/partials/Header.jsx
@@ -3,6 +3,14 @@ function Header () {
   return (
     <header className="site-header">
       <a href="/" className="logo">Knowledge Demonstration</a>
+      <nav>
+        <ul className="primary-nav">
+          <li><a href="#">One</a></li>
+          <li><a href="#">Two</a></li>
+          <li><a href="#">Three</a></li>
+          <li><a href="#">Four</a></li>
+        </ul>
+      </nav>
     </header>
   )
 }
